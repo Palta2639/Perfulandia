ce713df0deaaa94f5c127df2784f6d442e8daa35
Palta2639 <134703201+Palta2639@users.noreply.github.com>
2025-10-22 20:01:41 -0300

    Ventas_Microservicio #1
    Primer subida de archivos generales de el Microservicio_Ventas

---
+++ src/main/java/Ventas_Microservicio/Ventas/Repository/VentasRepository.java
@@ -0,0 +1,5 @@
+package Ventas_Microservicio.Ventas.Repository;
+
+public class VentasRepository {
+    
+}
---
+++ src/test/java/Ventas_Microservicio/Ventas/VentasApplicationTests.java
@@ -0,0 +1,13 @@
+package Ventas_Microservicio.Ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
